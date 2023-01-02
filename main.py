b0fd65dd1d4b810c184de41a85644910569f6d2f
BastionAtackDev <sbypsm@gmail.com>
2023-01-02 15:38:39 +0200

    Create JSON-editor

---
+++ main.py
@@ -0,0 +1,43 @@
+fileName = input("How is your file called: ")
+while(True):
+    line = "{"
+    inputText = input("What do you want to do(1=read the file, 2=write in the file, 3=create the file, exit=exit): ")
+    if (inputText == "1"):
+        file = open(fileName,"r")
+        print(file.read())
+        file.close()
+
+    if (inputText == "2"):
+        con = "y"
+        content = ""
+        name = ""
+        value = ""
+        file = open(fileName, "r")
+        line = file.read().replace("}",",")
+        file = open(fileName,"w")
+        # if (file):
+        #     line = ""
+        #     content = file.read().replace("}", ",")
+        #     file.close()
+        #     file = open(fileName,"w")
+        #     file.write(content)
+        while (con == "y"):
+
+            name = input("name?")
+            value = input("value?")
+            line =f"{line} \"{name}\" : \"{value}\""
+            con = input("Do you want to continue(y/n): ")
+            if (con == "y"):
+                line = line + ","
+        line = line + "}"
+        file.write(line)
+        file.close()
+
+    if (inputText == "3"):
+        file = open(fileName, "w")
+        line = "{"
+        file.write(line)
+        file.close()
+
+    if (inputText == "exit"):
+        break
